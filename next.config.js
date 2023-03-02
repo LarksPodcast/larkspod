200c86bddfa20616dedfe748970e4b9a2ae26b94
romeo <romeomeshack34@gmail.com>
2023-03-02 13:49:54 +0100

    Modified

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
+const { parsed: localEnv } = require("dotenv").config({
+  path: path.resolve(__dirname, `.env.local`),
+});
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: localEnv,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+module.export = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src/styles")],
+  },
+};

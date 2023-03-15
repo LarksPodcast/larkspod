455c9a6e1e74e67fdaa29d85bfad7434246e281e
romeo <romeomeshack34@gmail.com>
2023-03-15 16:17:08 +0100

    modified

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,5 +1,7 @@
 export default function Contact() {
   return (
-    <div>Contact Larks Podcast.</div>
+    <main className="h-screen">
+      <p>Larks Podcast contact page.</p>
+    </main>
   )
 }
--- src/pages/episodes/index.tsx
+++ src/pages/episodes/index.tsx
@@ -1,5 +1,19 @@
-export default function Episodes() {
+import { getPodcastEpisodes, graphQLRequest } from "@/graphQL/gql";
+
+type dataType = {
+  data: any;
+  podcastSeries: any;
+};
+
+export default function Episodes({ data, podcastSeries }: dataType) {
+
   return (
-    <div>Larks Podcast episodes</div>
-  )
+    <main className="h-screen">
+      <p>Larks Podcast episodes page</p>
+    </main>
+  );
 }
+
+/*
+  - Present podcast episodes page
+*/

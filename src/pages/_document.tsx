855692d5aa8c1aff2e64a13008502f57921b0070
romeo <romeomeshack34@gmail.com>
2023-03-20 17:29:58 +0100

    Added icons and font external links

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,24 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=optional"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Knewave&family=Poppins:wght@100;200;300;400;500;600&family=Shadows+Into+Light&family=Yellowtail&display=swap"
+        />
+      </Head>
+      <Script />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }

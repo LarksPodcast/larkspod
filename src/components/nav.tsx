414925bbadeaa7fe17ce984b2e337ed11e577fd6
romeo <romeomeshack34@gmail.com>
2023-03-24 19:03:41 +0100

    Navigation menu

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,19 +1,37 @@
+import React from "react";
+import MaterialIcon from "./materialIcon";
+
 import Link from "next/link";
+import Image from "next/image";
+
+import larkspodcastLogo from "../assets/images/larks-logo.svg";
 
 export default function Nav() {
   return (
-    <nav className="mb-10 p-5">
-      <ul className="flex justify-evenly w-full">
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/episodes">Episodes</Link>
-        </li>
-        <li>
-          <Link href="/contact">Contact</Link>
-        </li>
-      </ul>
-    </nav>
+    <div id="larkspodcast-nav-container">
+      <div id="logo-container" className="w-full py-5 px-5">
+        <Image src={larkspodcastLogo} alt="Larks podcast logo" />
+      </div>
+
+      <nav className="mb-10 p-5">
+        <ul className="flex justify-evenly text-white custom-bg-color-secondary w-[22rem] sm:w-[30rem] py-2 rounded-full">
+          <li>
+            <Link href="/" className="h-full block">
+              <MaterialIcon iconName="home" className="text-5xl" />
+            </Link>
+          </li>
+          <li>
+            <Link href="/episodes" className="block h-full">
+              <MaterialIcon iconName="keyboard_voice" className="text-5xl" />
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact" className="block h-full">
+              <MaterialIcon iconName="alternate_email" className="text-5xl" />
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }

45c40ed1e877c3984729e3cbbf58db8acaf7e52e
romeo <romeomeshack34@gmail.com>
2023-04-18 15:36:11 +0100

    Footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -12,34 +12,77 @@ export default function Footer() {
   const Year = date.getFullYear();
 
   return (
-    <footer className="custom-bg-color-accent pt-10 sm:pt-20 pb-5 px-4 sm:px-10 font-poppins h-[370px]">
-        <div className="footer-details-container flex flex-col sm:flex-row justify-around items-start mb-10 sm:mb-20">
-          <div className="footer-dates&socials text-white w-full sm:w-[300px] max-w-[300px]">
-            <h3 className="text-2xl font-medium py-5">LARKS - &copy;{Year}</h3>
-            <div className="socials-contact w-[75%]">
-              <div className="w-full flex justify-between">
-                <Link href="http://instagram.com/larkspod" className="social-contact-link" target="_blank">
-                  <Image src={Instagram} alt="Instagram contact" title="Check our feed and send us a DM" />
-                </Link>
-                <Link href="https://twitter.com/larkspod" className="social-contact-link" target="_blank">
-                  <Image src={Twitter} alt="Twitter contact" className="w-full" title="Tweet at us" />
-                  </Link>
-                <Link href="https://youtube.com/@larkspodcast" className="social-contact-link" title="Watch, like and subscribe" target="_blank">
-                  <Image src={Youtube} alt="YouTube contact" className="w-full" />
-                </Link>
-                <Link href="mailto:larkspodcast@gmail.com" className="social-contact-link" title="Send us an email" target="_blank">
-                  <Image src={Email} alt="Email contact" className="" />
-                </Link>
-              </div>
+    <footer className="custom-bg-color-accent pt-10 sm:pt-20 pb-5 px-4 sm:px-10 font-poppins h-auto">
+      <div className="footer-details-container flex flex-col sm:flex-row justify-around items-start mb-10 sm:mb-20">
+        <div className="footer-dates&socials text-white w-full sm:w-[300px] max-w-[300px]">
+          <h3 className="text-2xl font-medium py-5">LARKS - &copy;{Year}</h3>
+          <div className="socials-contact w-[75%]">
+            <div className="w-full flex justify-between">
+              <Link
+                href="http://instagram.com/larkspod"
+                className="social-contact-link"
+                target="_blank"
+              >
+                <Image
+                  src={Instagram}
+                  alt="Instagram contact"
+                  title="Check our feed and send us a DM"
+                />
+              </Link>
+              <Link
+                href="https://twitter.com/larkspod"
+                className="social-contact-link"
+                target="_blank"
+              >
+                <Image
+                  src={Twitter}
+                  alt="Twitter contact"
+                  className="w-full"
+                  title="Tweet at us"
+                />
+              </Link>
+              <Link
+                href="https://youtube.com/@larkspodcast"
+                className="social-contact-link"
+                title="Watch, like and subscribe"
+                target="_blank"
+              >
+                <Image src={Youtube} alt="YouTube contact" className="w-full" />
+              </Link>
+              <Link
+                href="mailto:larkspodcast@gmail.com"
+                className="social-contact-link"
+                title="Send us an email"
+                target="_blank"
+              >
+                <Image src={Email} alt="Email contact" className="" />
+              </Link>
             </div>
           </div>
-          <div className="footer-newsletter w-full sm:w-96">
-            <h4 className="text-[1.2rem] md:text-2xl lg:text-3xl font-bold py-5 text-white">Sign up to the newsletter</h4>
-            <NewsletterInput />
-          </div>
         </div>
-        <hr className="border-orange-300 mb-2" />
-        <p className="font-light text-center text-white">Engineered by the <Link href="https://duowork.github.io" className="!font-semibold" target="_blank">Duowork</Link> team.</p>
+        <div className="footer-newsletter w-full sm:w-[30rem]">
+          <h4 className="text-[1.2rem] md:text-2xl lg:text-3xl font-bold py-5 text-white">
+            Do you classify as a Gen Z?
+          </h4>
+          <p className="text-black font-poppins font-medium mb-5">
+            Sign up to my newsletter to be the first to know about podcast
+            updates, and if I ever fee like speaking to you... lmao.
+          </p>
+          <NewsletterInput />
+        </div>
+      </div>
+      <hr className="border-orange-300 mb-2" />
+      <p className="font-light text-center text-white">
+        Engineered by the{" "}
+        <Link
+          href="https://duowork.github.io"
+          className="!font-semibold underline"
+          target="_blank"
+        >
+          Duowork
+        </Link>{" "}
+        team.
+      </p>
     </footer>
-  )
+  );
 }

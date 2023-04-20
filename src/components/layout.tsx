10ef0e537be0fc58de6c8a67a821d4c9776d6994
romeo <romeomeshack34@gmail.com>
2023-04-20 14:03:41 +0100

    Layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -37,25 +37,31 @@ export default function Layout({ children }: any) {
   if (error) {
     return (
       <main className="h-screen">
-         <SEO title="Larks Podcast | Error" />
-       <div id="network-error-prompt" className="h-full flex justify-center mt-[25rem] p">
-       <p className="custom-text-color-primary text-2xl sm:text-3xl mx-10 md:mx-20 lg:mx-[15rem] text-center leading-tight">
-          There seem to be a network error! Check that you're connected
-          to the internet or try again later
-        </p>
-       </div>
+        <SEO title="Larks Podcast | Error" />
+        <div
+          id="network-error-prompt"
+          className="h-full flex justify-center mt-[25rem] p"
+        >
+          <p className="custom-text-color-primary text-2xl sm:text-3xl mx-10 md:mx-20 lg:mx-[15rem] text-center leading-tight">
+            There seem to be a network error! Check that you're connected to the
+            internet or try again later
+          </p>
+        </div>
       </main>
     );
   }
 
   if (podcastSeries === undefined) {
-    return (<Loader />);
+    return <Loader />;
   }
 
   return (
     <Fragment>
-      <SEO title="Larks Podcast" description="LARKS is a podcast that defies the norms of being specific, straightforward and concise; It embraces the ridiculous, the silly and the superficial and It’s more about the laffs than the feels." />
-     {router.pathname === "/"?null: <Nav />}
+      <SEO
+        title="Larks Podcast"
+        description="LARKS is a podcast that defies the norms of being specific, straightforward and concise; It embraces the ridiculous, the silly and the superficial and It’s more about the laffs than the feels."
+      />
+      {router.pathname === "/" ? null : <Nav />}
       {podcastSeries !== undefined
         ? cloneElement(children, {
             podcastSeries: podcastSeries,

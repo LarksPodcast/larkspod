25eddd454c47470b2c95754f1bd595ab198face6
romeo <romeomeshack34@gmail.com>
2023-04-20 14:16:22 +0100

    Added code to hide nav when scrolled to bottom

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -7,6 +7,24 @@ import Image from "next/image";
 import larkspodcastLogo from "../assets/images/larks-logo.svg";
 
 export default function Nav() {
+  const [showNav, setShowNav] = React.useState(false);
+
+  const browserWindow = window !== undefined;
+
+  if (browserWindow) {
+    window.addEventListener("scroll", () => {
+      if (
+        window.innerHeight + window.pageYOffset >=
+        document.body.offsetHeight
+      ) {
+        setShowNav(true);
+      } else {
+        setShowNav(false);
+      }
+    })
+
+  }
+
   return (
     <div id="larkspodcast-nav-container">
       <div id="logo-container" className="w-full py-5 px-5">
@@ -16,7 +34,11 @@ export default function Nav() {
       </div>
 
       <nav className="mb-10 p-5">
-        <ul className="flex justify-evenly text-white custom-bg-color-secondary w-[22rem] sm:w-[30rem] py-2 rounded-full border-2 border-white">
+        <ul
+          className={`${
+            showNav ? "!hidden" : "flex"
+          } justify-evenly text-white custom-bg-color-secondary w-[22rem] sm:w-[30rem] py-2 rounded-full border-2 border-white`}
+        >
           <li>
             <Link href="/" className="h-full block">
               <MaterialIcon iconName="home" className="text-5xl" />

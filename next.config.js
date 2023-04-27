61714c51cd7c3a037f4908010fb17a015d239304
romeo <romeomeshack34@gmail.com>
2023-04-27 11:32:59 +0100

    Config

--- next.config.js
+++ next.config.js
@@ -7,16 +7,25 @@ const { parsed: localEnv } = require("dotenv").config({
 
 const nextConfig = {
   reactStrictMode: true,
+  // Set up environment variables
   env: localEnv,
+  // Set up image optimization
   images: {
-    domains: ["d3t3ozftmdmh3i.cloudfront.net"]
-  }
-};
-
-module.exports = nextConfig;
-
-module.export = {
+    domains: ["d3t3ozftmdmh3i.cloudfront.net"],
+  },
+  // SASS
   sassOptions: {
     includePaths: [path.join(__dirname, "src/styles")],
   },
+  // Bypass CORS
+  async rewrites() {
+    return [
+      {
+        source: "/api/subscriber",
+        destination: "https://api.getresponse.com/contacts",
+      },
+    ];
+  },
 };
+
+module.exports = nextConfig;

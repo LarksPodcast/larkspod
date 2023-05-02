bcf22510f17f1d2a435adfdb3c5db68ca75439b9
romeo <romeomeshack34@gmail.com>
2023-05-02 10:17:03 +0100

    Modified component

--- src/components/materialIcon.tsx
+++ src/components/materialIcon.tsx
@@ -1,20 +1,41 @@
 import React from "react";
+import Image from "next/image";
 
 type IconPropType = {
-  iconName: string;
+  iconName?: string | undefined;
+  iconObject?: any | undefined;
   className?: string;
   onClick?: (arg: any) => any;
 };
 
 export default function MaterialIcon({
   iconName,
+  iconObject,
   className,
   onClick,
 }: IconPropType) {
+
+  if (iconObject) {
+    return (
+      <div className="inline-block">
+        <span
+          className={`material-symbols-rounded block ${
+            className ? className : ""
+          }`}
+          onClick={onClick ? onClick : undefined}
+        >
+          <Image src={iconObject} alt="gg" />
+        </span>
+      </div>
+    ); 
+  }
+
   return (
     <div className="inline-block">
       <span
-        className={`material-symbols-rounded block ${className ? className : ""}`}
+        className={`material-symbols-rounded block ${
+          className ? className : ""
+        }`}
         onClick={onClick ? onClick : undefined}
       >
         {iconName}

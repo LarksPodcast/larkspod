c5f36d03d441b3080dfe32635e4df6320a5e0305
romeo <romeomeshack34@gmail.com>
2023-05-08 13:53:12 +0100

    Page document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,14 @@
-import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0&display=swap"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />

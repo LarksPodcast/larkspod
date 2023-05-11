2f38fdee32eb23b7ea6f0bcd7af1db3e06f3c371
romeo <romeomeshack34@gmail.com>
2023-05-11 15:19:13 +0100

    Aded meta tags to preview links

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -8,12 +8,29 @@ type SEOType = {
 };
 
 export default function SEO({ title, description, children }: SEOType) {
+  const websiteDescription = `The ridiculous, silly and apologetically superficial podcast that is more about the laughs than the feels.`;
+
   return (
     <Head>
       <title>{title || ""}</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="description" content={description || `The ridiculous, silly and apologetically superficial podcast that is more about the laughs than the feels.`} />
       <link rel="icon" href="/favicon.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content={description || websiteDescription} />
+
+      {/* OG for facebook */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://larkspdocast.com" />
+      <meta property="og:title" content="larkspdocast" />
+      <meta property="og:description" content={websiteDescription} />
+      <meta property="og:image" content="/banner.png" />
+
+      {/* Twitter */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://larkspdocast.com" />
+      <meta property="twitter:title" content="larkspdocast" />
+      <meta property="twitter:description" content={websiteDescription} />
+      <meta property="twitter:image" content="/banner.png" />
+
       {children}
     </Head>
   );

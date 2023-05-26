e31508b0aef4451922959d1843c0c5b9c138c329
romeo <romeomeshack34@gmail.com>
2023-05-26 14:50:05 +0100

    Added OG meta tags for link preview

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+import banner from "../assets/images/banner.png"
 
 type SEOType = {
   title: string;
@@ -8,6 +9,8 @@ type SEOType = {
 };
 
 export default function SEO({ title, description, children }: SEOType) {
+  console.log(banner);
+
   const websiteDescription = `The ridiculous, silly and apologetically superficial podcast that is more about the laughs than the feels.`;
 
   return (
@@ -22,16 +25,20 @@ export default function SEO({ title, description, children }: SEOType) {
       {/* OG for facebook */}
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://larkspdocast.com" />
-      <meta property="og:title" content="larkspdocast" />
+      <meta property="og:title" content="Larks podcast" />
+      <meta property="og:site_name" content="Larks podcast" />
       <meta property="og:description" content={websiteDescription} />
-      <meta property="og:image" content="/banner.png" />
+      <meta property="og:image" content={banner.src} />
+      <meta property="og:image:alt" content="Larks podcast banner" />
 
       {/* Twitter */}
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://larkspdocast.com" />
-      <meta property="twitter:title" content="larkspdocast" />
+      <meta property="twitter:title" content="Larks podcast" />
       <meta property="twitter:description" content={websiteDescription} />
-      <meta property="twitter:image" content="/banner.png" />
+      <meta property="twitter:site" content="@larkspod" />
+      <meta property="twitter:url" content="https://larkspdocast.com" />
+      <meta property="twitter:image" content={banner.src} />
+      <meta property="twitter:image:alt" content="Larks podcast banner" />
 
       {children}
     </Head>
